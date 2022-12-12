6b30cce1c47bde36118943129d0596a485db4b32
Antonio <73387462+antonio-vachev@users.noreply.github.com>
2022-12-12 15:04:27 +0200

    Path update

---
+++ PIPA.extension/PIPA.tab/BIM Data.panel/col1.stack/BIM Bank Files.pulldown/EL.pushbutton/BankFileEL_script.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*- 
+
+import os
+import logexporter
+from pyrevit import forms
+from pyrevit.revit import ErrorSwallower
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
+from Autodesk.Revit.UI.Events import *
+
+# select folder of files to be updated
+directoryContents = r"IPA-DATA-BANK-EL-Central.rvt"
+filesFolder = r"B:\\01. BIM DATA\\02. BANK FILES\\"
+
+tOptions = TransactWithCentralOptions()
+revitModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(os.path.join(filesFolder, directoryContents))
+
+# define open options so that Revit automatically closes all worksets and detaches the file as central upon opening
+openOptions = OpenOptions()
+openOptions.DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets
+# openConfig = WorksetConfiguration(WorksetConfigurationOption.CloseAllWorksets)
+# openOptions.SetOpenWorksetsConfiguration(openConfig)
+
+# open Revit document
+revitDocument = __revit__.OpenAndActivateDocument(revitModelPath, openOptions, True)
+
+# Export log file
+current_file = __file__.split("\\")
+logexporter.logExport(current_file)
--- PIPA.extension/PIPA.tab/BIM Info.panel/col1.stack/BIM Documents.pulldown/03_Annotations.pushbutton/script.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-# os.system("start \"\" http://moodle.ip-arch.com/moodle/pluginfile.php/574/mod_resource/content/1/02%20-%20IPA%20-%20Windows%20panel.mp4")
-
-webbrowser.open(r"B:\\03. TRAINING VIDEOS & RESOURCE\\03. REVIT TRAINING\\Training Videos\\Revit_tutorials_BG_Aula\\")
